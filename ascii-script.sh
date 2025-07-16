4b9eefb7287b2422208f9ec8e85a195ca673f708
sreni <33428674+sreni@users.noreply.github.com>
2025-07-16 17:09:01 +0530

    rmoved not cond with feature

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,4 +1,4 @@
 sudo apt update && sudo apt install -y cowsay
-cowsay -f pig "I have Teeths and and Trunk and i am Big" >> elephant.txt
+cowsay -f pig "I have Teeths and and Trunk and i am Big somuch" >> elephant.txt
 cat elephant.txt
 ls -ltra
